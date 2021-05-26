c0aec85a56af8e1de1ba3a09eabf326a2541e612
Vincent Tran <vincentuong@yahoo.com>
2021-05-26 03:08:51 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,11 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   
 contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      binding.pry
-    end
-  end
+  binding.pry
 end
 
 end
